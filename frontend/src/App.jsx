3916002496aab59246e6fa0fe350ab842daf0215
GiharaNavindu <141494979+GiharaNavindu@users.noreply.github.com>
2024-07-03 16:35:08 +0530

    front/package.json checked
    frontend/src/App.jsx
    frontend/src/main.jsx
    
    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,53 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route,Navigate  } from 'react-router-dom';
-// import AddGarbageDetails from './components/Pages/AddGarbageDetails.jsx'
-import Signin from './components/Pages/Signin.jsx'
-import Signup from './components/Pages/Signup.jsx'
-import {Button} from './components/ui/button.jsx'
-import AddGarbageDetails from './components/Pages/AddGarbageDetails'
-import React from 'react'
+// import { useState } from 'react'
+// import { BrowserRouter as Router, Routes, Route,Navigate  } from 'react-router-dom';
+// // import AddGarbageDetails from './components/Pages/AddGarbageDetails.jsx'
+// import Signin from './components/Pages/Signin.jsx'
+// import Signup from './components/Pages/Signup.jsx'
+// import {Button} from './components/ui/button.jsx'
+// import AddGarbageDetails from './components/Pages/AddGarbageDetails'
+// import React from 'react'
+// const App = () => {
+//   return (
+//     <>
+//       {/* <Routes>
+//           <Route  path="/add-garbage-details" element={<AddGarbageDetails />} />
+//       </Routes> */}
+
+//     <Router>
+//       <div>
+//         <Routes>
+//           <Route path="/" element={<Navigate to="/signin" />} />
+//           <Route path="/signin" element={<Signin />} />
+//           <Route path="/signup" element={<Signup />} />
+//           <Route path="/add-garbage-details" element={<AddGarbageDetails />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Signin from './components/Pages/Signin.jsx';
+import Signup from './components/Pages/Signup.jsx';
+import AddGarbageDetails from './components/Pages/AddGarbageDetails.jsx';
+
 const App = () => {
   return (
-    <>
-      {/* <Routes>
-          <Route  path="/add-garbage-details" element={<AddGarbageDetails />} />
-      </Routes> */}
-
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/signin" />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/add-garbage-details" element={<AddGarbageDetails />} />
-        </Routes>
-      </div>
-    </Router>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/add-garbage-details" element={<AddGarbageDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;

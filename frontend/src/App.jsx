6de4191eaf1d6c31cd801d077a70b81e84c71166
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-07-04 10:20:33 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react';
+<<<<<<< Updated upstream
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Map2 from './components/Pages/Map-test'
 import Signin from './components/Pages/Signin';
@@ -7,6 +8,14 @@ import Signup from './components/Pages/Signup';
 import AddGarbageDetails from './components/Pages/AddGarbageDetails';
 import HomeLanding from './components/Pages/HomeLanding';
 import Dashboard from './components/Pages/Dashboard';
+=======
+import { Routes, Route } from 'react-router-dom';
+import AddGarbageDetails from '@/components/Pages/AddGarbageDetails';
+import Map2 from '@/components/Pages/Map-test'
+import Signin from '@/components/Pages/Signin';
+import Signup from '@/components/Pages/Signup';
+import HomeLanding from '@/components/Pages/HomeLanding';
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -14,7 +23,11 @@ const App = () => {
       <Routes>
         <Route  path="/test/map" element={<Map2 />} />
         <Route path="/" element={<HomeLanding />} />
+<<<<<<< Updated upstream
         <Route path="/signin/:role"  element={<Signin />} />
+=======
+        <Route path="/signin" element={<Signin />} />
+>>>>>>> Stashed changes
         <Route path="/signup" element={<Signup />} />
         <Route path="/add-garbage-details" element={<AddGarbageDetails />} />
         <Route path="/dashboard/:id" element={<Dashboard />} />
